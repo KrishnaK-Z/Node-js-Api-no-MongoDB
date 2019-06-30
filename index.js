7ff390763278e2c7a76eec4829df5338e46135e7
krishnaMsec <krishnakumar1171998@gmail.com>
2019-06-30 10:21:25 +0530

    express-handlebars

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const path = require('path');
+const exphs = require('express-handlebars');
+const companies = require('./Companies');
+
 const logger = require('./middleware/logger');
 
 const app = express();
+
+// handlebars middleware
+app.engine('handlebars', exphs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false }));
 // Init middleware
 // app.use(logger);
+
+app.get('/', (request, response)=>{
+    response.render('index', {
+        title: "Companies App",
+        companies
+    });
+});
 
 // set the static folder
 app.use(express.static(path.join(__dirname,'public')));
